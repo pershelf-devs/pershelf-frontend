f86861fa4c6533e2e14f84c06a0a0e3916aa6000
Samet-Hodaman <samethodaman1@gmail.com>
2025-03-19 00:38:50 +0300

    Backend config eklendi

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,52 +1,104 @@
 import { Checkbox, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, TextField } from "@mui/material";
 import React, { useState } from "react";
 import Register from "./Register";
+import axios from "axios";
 
 export default function Login() {
-  const [open, setOpenLogin] = useState(false);
+  const [open, setLoginModal] = useState(false);
   const [openRegister, setOpenRegister] = useState(false);
+  const [userLoginInfo, setUserLoginInfo] = useState({
+    username: "",
+    password: "",
+    email: "",
+  })
 
-  const handleLoginOpen = () => setOpenLogin(true);
-  const handleLoginClose = () => setOpenLogin(false);
-
-  const handleRegisterOpen = () => {
-    setOpenLogin(false); 
+  const handleOpenRegisterModal = () => {
+    setLoginModal(false);
     setOpenRegister(true);
   };
 
-  const handleRegisterClose = () => setOpenRegister(false);
   const handleBackToLogin = () => {
     setOpenRegister(false);
-    setOpenLogin(true); 
+    setLoginModal(true);
   };
+
+  const handleUserInfoChange = (e) => {
+    setUserLoginInfo({
+      ...userLoginInfo,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const loginURL = "http://localhost:443/api/auth/login";
+
+  const handleLogin = async () => {
+    const res = await axios.get(loginURL, {
+      params: {
+        email: userLoginInfo.email,
+        password: userLoginInfo.password,
+      }
+    })
+
+    // Eğer okey ise user bilgileri ve access token döner.
+    // Eğer değil ise hata mesajı döner, kullanıcıya gösterirsin.
+
+    console.log(res);
+  }
 
   return (
     <div className="flex justify-end p-4 text-white">
-      <Button variant="contained" color="primary" onClick={handleLoginOpen}>
+      <Button variant="contained" color="primary" onClick={() => setLoginModal(true)}>
         Sign In
       </Button>
 
-      <Dialog open={open} onClose={handleLoginClose}>
+      <Dialog open={open} onClose={() => setLoginModal(false)}>
         <DialogTitle className="text-center p-3 gap-10">Sign In</DialogTitle>
         <DialogContent className="flex flex-col gap-4 p-3">
-          <TextField placeholder="E-Mail" type= "email" variant="outlined" fullWidth autoFocus />
-          <TextField placeholder="Password" type="password" variant="outlined" fullWidth />
+          <TextField
+            placeholder="E-Mail"
+            type="email"
+            variant="outlined"
+            name="email"
+            fullWidth
+            autoFocus
+            value={userLoginInfo.email}
+            onChange={handleUserInfoChange}
+          />
+          <TextField
+            placeholder="Password"
+            type="password"
+            variant="outlined"
+            fullWidth
+            name="password"
+            value={userLoginInfo.password}
+            onChange={handleUserInfoChange}
+          />
           <div className="flex justify-between items-center">
             <FormControlLabel control={<Checkbox />} label="Remember Me" />
             <Button color="primary">Forgot Password?</Button>
           </div>
         </DialogContent>
         <DialogActions className="flex flex-col gap-2 px-6 pb-6">
-          <Button variant="contained" color="primary" fullWidth>
+          <Button 
+            variant="contained" 
+            color="primary" 
+            fullWidth
+            onClick={handleLogin}
+          >
             Login
           </Button>
-          <Button variant="outlined" color="secondary" fullWidth onClick={handleRegisterOpen}>
+          <Button variant="outlined" color="secondary" fullWidth onClick={handleOpenRegisterModal}>
             Sign Up
           </Button>
         </DialogActions>
+        <pre>
+          <code>
+            {JSON.stringify(userLoginInfo, null, 2)}
+          </code>
+        </pre>
       </Dialog>
-      
-      <Register open={openRegister} handleClose={handleRegisterClose} handleBackToLogin={handleBackToLogin} />
+
+      <Register open={openRegister} handleClose={() => setOpenRegister(false)} handleBackToLogin={handleBackToLogin} />
     </div>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs';
+import path from 'path';
+
+const configPath = path.resolve('/pershelf/etc/server.json');
+if (!fs.existsSync(configPath)) {
+  throw new Error(`Config file not found at ${configPath}`);
+}
+
+const configData = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
+if (!configData.server || !configData.server.serverIp || !configData.server.serverPort) {
+  throw new Error('Invalid server configuration.');
+}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +20,23 @@ export default defineConfig({
     tailwindcss(),
     react()
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: `https://${configData.server.serverIp}:${configData.server.serverPort}`,
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/restapi/v1.0'),
+      },
+    },
+    /*
+    https: {
+      key: fs.readFileSync(configData.server.serverKey),
+      cert: fs.readFileSync(configData.server.serverCrt),
+    },
+    */
+    strictPort: true,
+  },
 })
